39bd61a7e1f2cc191c84826bc507ab37fe64f798
NexWonka <willy77889@gmail.com>
2022-07-30 07:37:47 +0800

    add paralax effect using locomotive-scroll

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 import './style.css'
+import LocomotiveScroll from 'locomotive-scroll';
+
+let scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
